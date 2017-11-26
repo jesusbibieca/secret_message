4c9632246f086a0e5d816ced250e632afde37ace
Jesus Bibieca <jesusbibieca52@gmail.com>
2017-11-26 00:33:22 -0400

    fix bug with chcwd instead of chidr

--- rename_files.py
+++ rename_files.py
@@ -3,10 +3,10 @@ def rename_files():
     file_list = os.listdir(r"E:\Programing\Python\CS102\secret_message\prank")
     # return file_list
     cwd = os.getcwd()
-    os.chcwd(r"E:\Programing\Python\CS102\secret_message\prank")
+    os.chdir(r"E:\Programing\Python\CS102\secret_message\prank")
     for file_name in file_list:
         os.rename(file_name, file_name.translate(None, '0123456789'))
-    os.chcwd(cwd)
+    os.chdir(cwd)
     return "Files were renamed"
 
 print rename_files ()
