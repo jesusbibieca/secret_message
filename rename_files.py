7184ba07e2323dc97e6e96d393a4c7cea4ae9444
Jesus Bibieca <jesusbibieca52@gmail.com>
2017-11-26 00:31:05 -0400

    add rename funcionallity

--- rename_files.py
+++ rename_files.py
@@ -1,6 +1,12 @@
 import os
 def rename_files():
     file_list = os.listdir(r"E:\Programing\Python\CS102\secret_message\prank")
-    return file_list
-    
+    # return file_list
+    cwd = os.getcwd()
+    os.chcwd(r"E:\Programing\Python\CS102\secret_message\prank")
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, '0123456789'))
+    os.chcwd(cwd)
+    return "Files were renamed"
+
 print rename_files ()
